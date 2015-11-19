7b78cf83f08d3fba423e069d4cacd88a3cee716c
wb <519369865@qq.com>
2015-11-20 00:11:15 +0100

    finish PCA

--- R/pca.r
+++ R/pca.r
@@ -10,4 +10,15 @@ data <- reshape(data, idvar = "Date", timevar = "Stock", direction = "wide")
 #sort data by date, asc
 data <- arrange(data, Date)
 #apply PCA 
-pca.model = prcomp(data)
+pca.model = prcomp(data[,2:ncol(data)])
+#Get PCA component PCA component 1:
+PC1 <- pca.model$x[,"PC1"]
+#add id as duration days:
+duration <- 1:nrow(PC1)
+#transform into data frame and combine
+PC1 <- as.data.frame(PC1)
+duration <- as.data.frame(duration)
+PC1 <- cbind(PC1, duration)
+colnames(PC1) <- c("feature", "duration")
+#draw plot 
+pc1_plot <- qplot(PC1$duration, PC1$feature)
